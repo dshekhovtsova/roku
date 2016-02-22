002d7adb0619f6e2caf3d5f2faf06f5177f9bb08
Daria Shekhovtsova <daria.shekhovtsova@synapse.com>
2016-02-22 10:16:59 +0200

    22.02.2016

---
+++ src/gl/homework3/Employee.java
@@ -0,0 +1,38 @@
+package gl.homework3;
+
+public class Employee {
+private String firstName;
+private String lastName;
+private double salary;
+private int experience;
+
+public final String getFirstName() {
+	return firstName;
+}
+public final void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+public final String getLastName() {
+	return lastName;
+}
+public final void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+public final double getSalary() {
+	return salary;
+}
+public final void setSalary(double salary) {
+	this.salary = salary;
+}
+public final int getExperience() {
+	return experience;
+}
+public final void setExperience(int experience) {
+	this.experience = experience;
+}
+
+
+
+
+
+}
